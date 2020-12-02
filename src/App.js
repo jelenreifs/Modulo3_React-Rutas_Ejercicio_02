ad0affbc0e8fa2c1ec0dbd9ea9ca40959e88ae1a
REIFS HERNANDO <ereifs@serikat.es>
2020-12-02 08:22:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { useState } from "react";
+
+
+ function Cabecera() {
+    return (
+      <header>
+        <ul>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/nombres">Nombres</Link>
+          </li>
+        </ul>
+      </header>
+    )
+  }
+
+function Nombres() {
+  const [nombres, setNombres] = useState(["Ane", "Bea", "Lorea", "Julia", "Maider"]);
+    
+  const mostrarNombres = nombres.map(nombre => {
+    return (
+      <li>{nombre}</li>
+    )
+  })
+
+ /*  El return de nombres  */
+  return <ul>{ mostrarNombres}</ul>
+   
+}
+      
+      
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+      <BrowserRouter>
+       <Cabecera />
+      <Route exact path="/">
+         <h1>Rutas en React</h1>
+       </Route>
+         
+      <Route exact path="/nombres">
+         <Nombres />
+       </Route>
+    
+      </BrowserRouter>
+      
+    )
+  
 }
 
 export default App;
